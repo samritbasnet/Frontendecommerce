339666ae92a332cf78f0eb4fdd7e919e4b82dac7
Samrit Basnet <samritbasnet70@gmail.com>
2022-01-14 07:17:31 +0545

    first commit

---
+++ src/component/login/login.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class Login extends React.Component {
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Hello Sam</h1>
+            </div>
+        )
+    }
+} 
